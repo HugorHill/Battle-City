638366ac1643c2f446e97f0fe84472ef148e22ae
HugorHill <47428752+HugorHill@users.noreply.github.com>
2019-02-25 10:54:43 +0300

    F

--- Engine/PanzerPlayer.cpp
+++ Engine/PanzerPlayer.cpp
@@ -11,20 +11,24 @@ PanzerPlayer::PanzerPlayer(float x,float y,int dir,float vel,void*_p,void* _map)
 	cooldown = Panzer::std_cooldown;
 	engine = _p;
 	map = _map;
-	texture =  _ptr(engine,Engine)->mm.loadTexture("map/mainhero.jpg", true);
+	texture =  _ptr(engine,Engine)->mm.loadTexture("map/mainhero.png", true);
 }
 void PanzerPlayer::logic() {
 	if (glfwGetKey(_ptr(engine,Engine)->getWindow(),GLFW_KEY_W) == GLFW_PRESS) {
 		direction = 0;
+		velocity = std_vel;
 	}
 	if (glfwGetKey(_ptr(engine, Engine)->getWindow(), GLFW_KEY_D) == GLFW_PRESS) {
 		direction = 1;
+		velocity = std_vel;
 	}
 	if (glfwGetKey(_ptr(engine, Engine)->getWindow(), GLFW_KEY_S) == GLFW_PRESS) {
 		direction = 2;
+		velocity = std_vel;
 	}
 	if (glfwGetKey(_ptr(engine, Engine)->getWindow(), GLFW_KEY_A) == GLFW_PRESS) {
 		direction = 3;
+		velocity = std_vel;
 	}
 	if (glfwGetKey(_ptr(engine, Engine)->getWindow(), GLFW_KEY_SPACE) == GLFW_PRESS && cooldown <= 0) {
 	Panzer::Shoot();
